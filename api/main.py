23a6d9e84d4386ace63a822b2af7fe2101b5e84d
Shirisha <gugulothshirisha69@gmail.com>
2025-10-29 16:00:26 +0530

    FIX: Add Vercel URL to CORS origins

--- api/main.py
+++ api/main.py
@@ -11,6 +11,7 @@ app = FastAPI()
 origins = [
     "http://localhost",
     "http://localhost:3000",
+    "https://crop-vision-project.vercel.app", # <<< ADD THIS LINE
 ]
 app.add_middleware(
     CORSMiddleware,
